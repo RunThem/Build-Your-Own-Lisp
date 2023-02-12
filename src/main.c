a5b981e1408b1c461557b6e1d2be8cfaba9cf5c9
RunThem <iccy.fun@outlook.com>
2023-02-12 22:32:31 +0800

    build the framework. complete simple interactive functions.
    Signed-off-by: RunThem <iccy.fun@outlook.com>

--- src/main.c
+++ src/main.c
@@ -1,9 +1,34 @@
+#include <stdbool.h>
 #include <stdio.h>
-#include <stddef.h>
-#include <mpc.h>
+#include <string.h>
+
+/* clang-format off */
 #include <linenoise.h>
+#include <mpc.h>
+/* clang-format on */
+
+#define LISP_PROMPT "=> "
 
 int main(int argc, char** argv) {
-  printf("hello world!\n");
+  printf("lisp!\n");
+
+  while (true) {
+    const char* input = linenoise(LISP_PROMPT);
+
+    if (input[0] == '\0') {
+      continue;
+    }
+
+    if (strncmp(input, "quit", sizeof("quit")) == 0) {
+      break;
+    }
+
+    linenoiseHistoryAdd(input);
+
+    printf("%*c\'%s\'\n", (int)strlen(LISP_PROMPT), ' ', input);
+
+    free((void*)input);
+  }
+
   return 0;
 }
