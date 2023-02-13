ca5390796039001ccc42252f4338aa6b7ac21e17
RunThem <iccy.fun@outlook.com>
2023-02-13 09:53:31 +0800

    familiar with the `mpc` library usage and solve easter eggs.
    Signed-off-by: RunThem <iccy.fun@outlook.com>

--- src/main.c
+++ src/main.c
@@ -9,6 +9,38 @@
 
 #define LISP_PROMPT "=> "
 
+void test_mpc() {
+  mpc_parser_t* Adjective = mpc_new("adjective");
+  mpc_parser_t* Noun      = mpc_new("noun");
+  mpc_parser_t* Phrase    = mpc_new("phrase");
+  mpc_parser_t* Doge      = mpc_new("doge");
+
+  // 彩蛋
+  //
+  // number: <integer> '.' <float>;
+  // integer: [0-9][1-9]*;
+  // float: [1-9];
+  //
+  // url: <scheme> "://" <realm_name> <path>
+  // scheme: "https" | "http"
+  // realm_name: <word> '.' <word> ['.' <word>]*
+  // path: '/' | '/' <word> ['/' <word>]*
+  // word: [a-zA-Z]+
+  //
+  // not parse JSON
+  mpca_lang(MPCA_LANG_DEFAULT,
+            "adjective: \"wow\" | \"many\" | \"so\" | \"such\";"
+            "noun: \"lisp\" | \"language\" | \"book\" | \"build\" | \"c\";"
+            "phrase: <adjective> <noun>;"
+            "doge: <phrase>*;",
+            Adjective,
+            Noun,
+            Phrase,
+            Doge);
+
+  mpc_cleanup(4, Adjective, Noun, Phrase, Doge);
+}
+
 int main(int argc, char** argv) {
   printf("lisp!\n");
 
