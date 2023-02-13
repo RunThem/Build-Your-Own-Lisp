c66ad1137c24797efbafe681bbb63b0012c1f247
RunThem <iccy.fun@outlook.com>
2023-02-13 15:23:39 +0800

    refactoring: error handling.
    Signed-off-by: RunThem <iccy.fun@outlook.com>

--- src/main.c
+++ src/main.c
@@ -13,93 +13,109 @@
 #define LISP_PROMPT "> "
 
 /* Declare New lvl Struct */
-typedef struct {
+typedef struct lval {
   enum {
-    LVAL_NUM,
-    LVAL_ERR
+    LVAL_INTEGER,
+    LVAL_DECIMAL,
+    LVAL_ERR,
+    LVAL_SYM,
+    LVAL_SEXPR
   } type;
 
   union {
-    int64_t num;
-    enum {
-      LERR_DIV_ZERO,
-      LERR_BAD_OP,
-      LERR_BAD_NUM,
-    } err;
+    int64_t integer;
+    double decimal;
+
+    char* err;
+    char* sym;
+
+    struct {
+      int count;
+      struct lval** cell;
+    };
   };
-} Lval;
+} lval;
 
 mpc_parser_t *Number, *Operator, *Expr, *Lispy;
 
-Lval lval_num(int64_t x) {
-  Lval v = {
-      .type = LVAL_NUM,
-      .num  = x,
-  };
+/* Construct a pointer to a new Integer lval. */
+lval* lval_integer(int64_t x) {
+  lval* v    = (lval*)calloc(1, sizeof(lval));
+  v->type    = LVAL_INTEGER;
+  v->integer = x;
 
   return v;
 }
 
-Lval lval_err(int x) {
-  Lval v = {
-      .type = LVAL_ERR,
-      .err  = x,
-  };
+/* Construct a pointer to a new Error lval */
+lval* lval_err(const char* m) {
+  lval* v = (lval*)calloc(1, sizeof(lval));
+  v->type = LVAL_ERR;
+  v->err  = strdup(m);
 
   return v;
+}
+
+/* Construct a pointer to a new Symbol lval */
+lval* lval_sym(const char* m) {
+  lval* v = (lval*)calloc(1, sizeof(lval));
+  v->type = LVAL_SYM;
+  v->sym  = strdup(m);
+
+  return v;
+}
+
+/* A pointer to a new empty Sexpr lval */
+lval* lval_sexpr() {
+  lval* v  = (lval*)calloc(1, sizeof(lval));
+  v->type  = LVAL_SEXPR;
+  v->count = 0;
+  v->cell  = NULL;
+
+  return v;
+}
+
+void lval_del(lval* v) {
+  switch (v->type) {
+    case LVAL_INTEGER:
+      break;
+    case LVAL_DECIMAL:
+      break;
+    case LVAL_ERR:
+      free(v->err);
+      break;
+    case LVAL_SYM:
+      free(v->sym);
+      break;
+    case LVAL_SEXPR:
+      for (int i = 0; i < v->count; i++) {
+        lval_del(v->cell[i]);
+      }
+
+      free(v->cell);
+      break;
+    default:
+      break;
+  }
+
+  free(v);
 }
 
 /* Print an "Lval" */
-void lval_print(Lval v) {
-  switch (v.type) {
-    case LVAL_NUM:
-      printf("%li", v.num);
+void lval_println(lval* v) {
+  switch (v->type) {
+    case LVAL_INTEGER:
+      printf("%li", v->integer);
       break;
     case LVAL_ERR:
-      if (v.err == LERR_DIV_ZERO) {
-        printf("Error: Division by zero!");
-      } else if (v.err == LERR_BAD_OP) {
-        printf("Error: Invalid operator!");
-      } else if (v.err == LERR_BAD_NUM) {
-        printf("Error: Invalid Number!");
-      }
+      printf("%s", v->err);
       break;
 
     default:
       break;
   }
-}
 
-/* Print an "Lval" followed by a newline. */
-void lval_println(Lval v) {
-  lval_print(v);
   printf("\n");
-}
-
-int number_of_nodes(mpc_ast_t* t) {
-  if (t->children_num == 0) {
-    return 1;
-  }
-
-  int total = 1;
-  for (int i = 0; i < t->children_num; i++) {
-    total += number_of_nodes(t->children[i]);
-  }
-
-  return total;
-}
-
-int number_of_leaf_nodes(mpc_ast_t* t) {
-  if (t->children_num == 0) {
-    return 1;
-  }
-
-  int total = 0;
-  for (int i = 0; i < t->children_num; i++) {
-    total += number_of_leaf_nodes(t->children[i]);
-  }
-
-  return total;
 }
 
 void parser() {
@@ -125,60 +141,62 @@ void clean() {
   mpc_cleanup(4, Number, Operator, Expr, Lispy);
 }
 
-Lval eval_op(Lval x, char* op, Lval y) {
-  if (x.type == LVAL_ERR) {
+lval* eval_op(lval* x, char* op, lval* y) {
+  if (x->type == LVAL_ERR) {
     return x;
   }
 
-  if (y.type == LVAL_ERR) {
+  if (y->type == LVAL_ERR) {
     return y;
   }
 
   if (strcmp(op, "+") == 0) {
-    return lval_num(x.num + y.num);
+    return lval_integer(x->integer + y->integer);
   }
   if (strcmp(op, "-") == 0) {
-    return lval_num(x.num - y.num);
+    return lval_integer(x->integer - y->integer);
   }
   if (strcmp(op, "*") == 0) {
-    return lval_num(x.num * y.num);
+    return lval_integer(x->integer * y->integer);
   }
   if (strcmp(op, "/") == 0) {
-    return (y.num == 0) ? lval_err(LERR_DIV_ZERO) : lval_num(x.num / y.num);
+    return (y->integer == 0) ? lval_err("Error: Division by zero!") :
+                               lval_integer(x->integer / y->integer);
   }
   if (strcmp(op, "%") == 0) {
-    return (y.num == 0) ? lval_err(LERR_DIV_ZERO) : lval_num(x.num % y.num);
+    return (y->integer == 0) ? lval_err("Error: Division by zero!") :
+                               lval_integer(x->integer % y->integer);
   }
   if (strcmp(op, "^") == 0) {
-    return lval_num(pow(x.num, y.num));
+    return lval_integer(pow(x->integer, y->integer));
   }
   if (strcmp(op, "min") == 0) {
-    return x.num < y.num ? x : y;
+    return x->integer < y->integer ? x : y;
   }
   if (strcmp(op, "max") == 0) {
-    return x.num > y.num ? x : y;
+    return x->integer > y->integer ? x : y;
   }
 
-  return lval_err(LERR_BAD_OP);
+  return lval_err("Error: Invalid operator!");
 }
 
-Lval eval(mpc_ast_t* t) {
+lval* eval(mpc_ast_t* t) {
   /* If tagged as number retruen it directly. */
   if (strstr(t->tag, "number")) {
     errno  = 0;
     long x = strtol(t->contents, NULL, 10);
-    return (errno != ERANGE) ? lval_num(x) : lval_err(LERR_BAD_NUM);
+    return (errno != ERANGE) ? lval_integer(x) : lval_err("Error: Invalid Number!");
   }
 
   /* The operator is always second child. */
   char* op = t->children[1]->contents;
 
   /* We store the third child in `x`. */
-  Lval x = eval(t->children[2]);
+  lval* x = eval(t->children[2]);
 
   /* Unary operator */
   if (t->children_num == 4) {
-    x.num = -x.num;
+    x->integer = -x->integer;
     return x;
   }
 
@@ -211,6 +229,7 @@ int main(int argc, char** argv) {
     mpc_result_t result;
     if (mpc_parse("<stdin>", input, Lispy, &result)) {
       lval_println(eval(result.output));
+      mpc_ast_print(result.output);
       mpc_ast_delete(result.output);
     } else {
       mpc_err_print(result.error);
